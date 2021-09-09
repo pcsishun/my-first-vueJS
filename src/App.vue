3edb6966f93fbd71102cd1bed08c348f62ad69c8
pcsishun <pcsishun@Gmail.com>
2021-09-09 13:09:27 +0700

    updadte

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h3 align="center">My first Vue.js</h3>
-    <ul class="nav nav-tabs">
+    <!-- <ul class="nav nav-tabs">
       <li class="nav-item">
         <a class="nav-link active" aria-current="page" href="#/">Home</a>
       </li>
@@ -17,7 +17,23 @@
        <li class="nav-item" style="margin-left: 70%; ">
         <a class="nav-link" href="#/login">Login</a>
       </li>   
-    </ul>
+    </ul> -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
     <br>
     <router-view/>
 
